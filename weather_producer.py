7a7a70aa88667a05dba9c9d0dbc56e7c5dba3ac9
jadesola98 <77394784+jadesola98@users.noreply.github.com>
2021-09-19 16:56:40 +0100

    Update weather_producer.py

--- weather_producer.py
+++ weather_producer.py
@@ -1,5 +1,6 @@
 from kafka import KafkaProducer
 import requests
+from json import dumps
 
 
 kafka_data_producers = KafkaProducer(bootstrap_servers=['localhost:9092'],value_serializer=lambda x: dumps(x).encode('utf-8') )
